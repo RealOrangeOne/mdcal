f00938ea27d090fc75db54757a05df184e7ddf64
Jake Howard <git@theorangeone.net>
2019-07-14 00:03:27 +0100

    Open in browser

--- mdcal/server.py
+++ mdcal/server.py
@@ -1,12 +1,15 @@
 import http.server
 import os
 from pathlib import Path
+import webbrowser
 
 import click
 
 
-def serve_output(output_dir: Path):
+def serve_output(output_dir: Path, open_in_browser=True):
     server = http.server.HTTPServer(("", 8000), http.server.SimpleHTTPRequestHandler)
     click.echo("Serving {} on port 8000".format(output_dir))
     os.chdir(str(output_dir))
+    if open_in_browser:
+        webbrowser.open("http://localhost:8000")
     server.serve_forever()
