12228fd9ef9f7633b1efcb5bc07abb85f9fc6614
Jake Howard <git@theorangeone.net>
2019-07-13 21:19:16 +0100

    Parse metadata for event

--- mdcal/event.py
+++ mdcal/event.py
@@ -1,12 +1,27 @@
 from pathlib import Path
+import hashlib
+
+from dateutil.parser import parse
+from markdown import Markdown
 
 
 class Event:
     def __init__(self, path: Path):
         self.path = path
+        md = Markdown(extensions=["meta"])
+        self.content = md.convert(path.read_text())
+        self.metadata = md.Meta  # type: dict
+        self.name = self.metadata.pop("name")[0]
+        self.date = parse(self.metadata.pop("date")[0])
+
+    @property
+    def id(self):
+        hasher = hashlib.sha1()
+        hasher.update(str(self.path).encode())
+        return hasher.hexdigest()
 
     def __str__(self):
-        return str(self.path)
+        return "{}: {} @ {}".format(self.id, self.name, self.date)
 
 
 def get_events_in_dir(dir: Path):
