530dfeb1fa33c7ec1d522da8fca203b3cf4037e1
Jake Howard <git@theorangeone.net>
2019-07-13 21:37:36 +0100

    Generate a final calendar

--- mdcal/__main__.py
+++ mdcal/__main__.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import click
 
 from .event import get_events_in_dir
+from ics import Calendar
 
 
 @click.command()
@@ -14,5 +15,7 @@ from .event import get_events_in_dir
     "output_dir", type=click.Path(file_okay=False, writable=True, resolve_path=True)
 )
 def main(input_dir, output_dir):
+    calendar = Calendar(creator="mdcal")
     for event in get_events_in_dir(Path(input_dir)):
-        print(event.to_ics_event())
+        calendar.events.add(event.to_ics_event())
+    print(str(calendar))
